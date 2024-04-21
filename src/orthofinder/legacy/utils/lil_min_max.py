a4b74d72c03df87cbd01f71ae815e3bf4d9413a6
OrthoFinder <steven.kelly@biology.ox.ac.uk>
2024-04-21 13:44:41 +0100

    refactor: move unused lil_min and lil_max funcs to legacy

---
+++ src/orthofinder/legacy/utils/lil_min_max.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def lil_min(M):
+    n = M.shape[0]
+    mins = np.ones((n, 1), dtype = np.float64) * 9e99
+    for kRow in range(n):
+        values=M.getrowview(kRow)
+        if values.nnz == 0:
+            continue
+        mins[kRow] = min(values.data[0])
+    return mins 
+
+def lil_max(M):
+    n = M.shape[0]
+    maxes = np.zeros((n, 1), dtype = np.float64)
+    for kRow in range(n):
+        values=M.getrowview(kRow)
+        if values.nnz == 0:
+            continue
+        maxes[kRow] = max(values.data[0])
+    return maxes
--- src/orthofinder/tools/dendroblast.py
+++ src/orthofinder/tools/dendroblast.py
@@ -302,26 +302,6 @@ def GetRAMErrorText():
 
 # ==============================================================================================================================
 
-def lil_min(M):
-    n = M.shape[0]
-    mins = np.ones((n, 1), dtype = np.float64) * 9e99
-    for kRow in range(n):
-        values=M.getrowview(kRow)
-        if values.nnz == 0:
-            continue
-        mins[kRow] = min(values.data[0])
-    return mins 
-
-def lil_max(M):
-    n = M.shape[0]
-    maxes = np.zeros((n, 1), dtype = np.float64)
-    for kRow in range(n):
-        values=M.getrowview(kRow)
-        if values.nnz == 0:
-            continue
-        maxes[kRow] = max(values.data[0])
-    return maxes
-
 def lil_minmax(M):
     n = M.shape[0]
     mins = np.ones((n, 1), dtype = np.float64) * 9e99
