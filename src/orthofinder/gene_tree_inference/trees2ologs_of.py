455a730e8c81aead9af61a5b04ca25999fd759a1
OrthoFinder <steven.kelly@biology.ox.ac.uk>
2025-04-08 14:42:13 +0100

    refactor: fix --fix-file for resolved gene trees

--- src/orthofinder/gene_tree_inference/trees2ologs_of.py
+++ src/orthofinder/gene_tree_inference/trees2ologs_of.py
@@ -1574,14 +1574,14 @@ class TreeAnalyser(object):
             # don't relabel nodes, they've already been done
             
             # util.RenameTreeTaxa(recon_tree, self.reconTreesRenamedDir + "OG%07d_tree.txt" % iog, self.spec_seq_dict, qSupport=False, qFixNegatives=True)
-            
-            # util.RenameTreeTaxa(
-            #     recon_tree, 
-            #     self.reconTreesRenamedDir + "OG%07d.txt" % iog, 
-            #     self.spec_seq_dict, 
-            #     qSupport=False, 
-            #     qFixNegatives=True
-            # )
+            if self.fix_files:
+                util.RenameTreeTaxa(
+                    recon_tree, 
+                    self.reconTreesRenamedDir + "OG%07d.txt" % iog, 
+                    self.spec_seq_dict, 
+                    qSupport=False, 
+                    qFixNegatives=True
+                )
                     
             # recon_tree.delete_traverse()
             # if iog >= 0 and divmod(iog, 10 if self.nOgs <= 200 else 100 if self.nOgs <= 2000 else 1000)[1] == 0:
